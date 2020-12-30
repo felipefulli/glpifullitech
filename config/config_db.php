3ac6c63d22abe22ad4e78ba7d2d3910621f274cc
Felipe Fulli <felipefulli@hotmail.com>
2020-12-30 13:22:28 +0000

    update folder glpi

---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = 'localhost';
+   public $dbuser     = 'glpi';
+   public $dbpassword = 'iwklx099';
+   public $dbdefault  = 'glpi_9.5.2';
+}
